6be106639c84f364edf32991a231bfbc2b601715
Overload119 <budgeneration@gmail.com>
2013-02-28 13:28:04 -0500

    Add more migration stuff I forgot about

--- lib/generators/audited/templates/add_transaction_id_to_audits.rb
+++ lib/generators/audited/templates/add_transaction_id_to_audits.rb
@@ -1,9 +1,11 @@
 class <%= migration_class_name %> < ActiveRecord::Migration
   def self.up
     add_column :audits, :transaction_id, :string
+    add_index :audits, :transaction_id, :name => 'transaction_id', :unique => true
   end
 
   def self.down
+    remove_index :audits, :transaction_id, :name => 'transaction_id', :unique => true
     remove_column :audits, :transaction_id
   end
 end
