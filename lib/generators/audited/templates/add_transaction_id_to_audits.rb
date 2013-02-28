9f66a1029cade813b2d18c3bef15b5e4fd44bd50
Overload119 <budgeneration@gmail.com>
2013-02-28 11:37:39 -0500

    Add migration for transaction id

---
+++ lib/generators/audited/templates/add_transaction_id_to_audits.rb
@@ -0,0 +1,10 @@
+class <%= migration_class_name %> < ActiveRecord::Migration
+  def self.up
+    add_column :audits, :transaction_id, :string
+  end
+
+  def self.down
+    remove_column :audits, :transaction_id
+  end
+end
+
