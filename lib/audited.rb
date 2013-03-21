40dc34c12587933057b93cd553aea3cf3c276b3a
Overload119 <budgeneration@gmail.com>
2013-03-21 16:08:17 -0400

    Remove :created_at from ignored_attributes

--- lib/audited.rb
+++ lib/audited.rb
@@ -5,7 +5,7 @@ module Audited
     attr_accessor :ignored_attributes, :current_user_method, :transaction_id_method, :audit_class, :restoring, :organization_id_method
   end
 
-  @ignored_attributes = %w(lock_version created_at updated_at created_on updated_on)
+  @ignored_attributes = %w(lock_version updated_at created_on updated_on)
 
   @current_user_method = :current_user
   @organization_id_method = :organization_id
